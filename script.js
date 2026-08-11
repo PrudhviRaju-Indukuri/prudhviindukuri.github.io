93a103704e6e95b5d9bf6629fb1964403439d2e6
PrudhviRaju-Indukuri <prudhviraju.i@outlook.com>
2026-08-12 01:59:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,17 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Close menu when a link is clicked
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
